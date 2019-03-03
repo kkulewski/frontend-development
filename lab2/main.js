cf91592213997884dd45cade16a7213485cdd552
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2019-03-03 11:02:09 +0100

    Add bikes initial data

---
+++ lab2/main.js
@@ -0,0 +1,40 @@
+// ### DATA ###
+
+var bike1 = {
+    id: 1,
+    make: "Pinarello",
+    model: "One",
+    price: 500.0,
+    tandem: false,
+    colors: ["red", "green"],
+    shifter: {
+        make: "Shimano",
+        gears: 7 
+    }
+}
+
+var bike2 = {
+    id: 2,
+    make: "BMC",
+    model: "X1",
+    price: 2500.0,
+    tandem: false,
+    colors: ["white", "black", "blue"],
+    shifter: {
+        make: "FSA",
+        gears: 5
+    }
+}
+
+var bike3 = {
+    id: 3,
+    make: "BMC",
+    model: "X2",
+    price: 2500.0,
+    tandem: true,
+    colors: ["black"],
+    shifter: {
+        make: "FSA",
+        gears: 3
+    }
+}
