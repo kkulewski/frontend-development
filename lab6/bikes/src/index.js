894d0e796be89bf18e9eaee76522f8f91dfd0553
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2019-04-14 09:36:39 +0200

    Add moving logo

--- lab6/bikes/src/index.js
+++ lab6/bikes/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import logo from './logo.svg';
+import './App.css';
+
 function Message() {
-    return <h2>Hello world</h2>
+    return (
+    <div className="App-moveX">
+        <div className="App-moveY">
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<Message/>, document.getElementById("root"));
